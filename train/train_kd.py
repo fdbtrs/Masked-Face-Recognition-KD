9f925884f340d92c5d11e160935e45d8fc5c5506
Fadi Boutros <fdbtrs@hotmail.com>
2021-10-01 11:09:57 +0200

    update dataloader

--- train/train_kd.py
+++ train/train_kd.py
@@ -210,7 +210,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PoketNet Training with template knowledge distillation')
     parser.add_argument('--local_rank', type=int, default=0, help='local_rank')
-    parser.add_argument('--network_student', type=str, default="dartFaceNet", help="backbone of student network")
+    parser.add_argument('--network_student', type=str, default="iresnet100", help="backbone of student network")
     parser.add_argument('--network_teacher', type=str, default="iresnet100", help="backbone of teacher network")
     parser.add_argument('--loss', type=str, default="ArcFace", help="loss function")
     parser.add_argument('--pretrained_student', type=int, default=0, help="use pretrained student model for KD")
--- utils/dataset.py
+++ utils/dataset.py
@@ -109,3 +109,10 @@ class MXFaceDataset(Dataset):
 
     def __len__(self):
         return len(self.imgidx)
+
+arcface_ref_points = np.array([ [30.2946, 51.6963],
+                                [65.5318, 51.5014],
+                                [48.0252, 71.7366],
+                                [33.5493, 92.3655],
+                                [62.7299, 92.2041]
+                                ], dtype=np.float32 )
